a9850456acd2db9d06456285d00f304b22103f38
Alexander Sels <alexandersels@hotmail.com>
2019-11-03 17:37:15 +0100

    pom update

--- backend/src/main/java/com/test/project/repositories/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.test.project.repositories;
-
-import com.test.project.entities.Name;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository()
-public interface TestRepository extends JpaRepository<Name, Long> {
-}
